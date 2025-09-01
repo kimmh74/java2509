9da5ab538adb4a6bc9a854ac55ef388d8468cf1a
kimmh74 <zziipp.mw@gmail.com>
2025-09-01 17:32:25 +0900

    1
    1

---
+++ ch02/sec02/LongExample.java
@@ -0,0 +1,19 @@
+package ch02.sec02;
+
+public class LongExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long var1 = 10;
+		long var2 = 20L;
+//		long var3 = 100000000000;
+		long var4 = 100000000000L;
+		//100000000L(마지막에 L을 넣어서 long L형)
+		
+		System.out.println(var1);
+		System.out.println(var2);
+//		System.out.println(var3);
+		System.out.println(var4);
+	}
+
+}
