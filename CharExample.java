c3f9ffc1f3e22bbfc80d5feb4de1b832bc482034
kimmh74 <zziipp.mw@gmail.com>
2025-09-01 15:35:24 +0900

    1
    1

---
+++ CharExample.java
@@ -0,0 +1,22 @@
+package ch02.sec01;
+
+public class CharExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char c1 ='A';
+		char c2 = 65;
+		
+		char c3 = '가';
+		char c4 = 44032;
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		System.out.println(c4);
+		
+		
+	}
+
+}
